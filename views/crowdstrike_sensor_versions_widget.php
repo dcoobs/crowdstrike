8d696e6c0fbb3ccfe306f559c502032538ef993e
dcoobs <12276948+dcoobs@users.noreply.github.com>
2019-12-10 12:48:38 -0600

    Add tooltip to sensor_versions widget

--- views/crowdstrike_sensor_versions_widget.php
+++ views/crowdstrike_sensor_versions_widget.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4 col-md-6">
         <div class="panel panel-default" id="crowdstrike-sensor-versions-widget">
-            <div class="panel-heading">
+            <div class="panel-heading" data-container="body" data-i18n="[title]crowdstrike.sensor-versions-widget-tooltip">
                 <h3 class="panel-title"><i class="fa fa-crosshairs"></i>
                     <span data-i18n="crowdstrike.sensor-versions-widget"></span>
                     <list-link data-url="/show/listing/crowdstrike/crowdstrike"></list-link>
