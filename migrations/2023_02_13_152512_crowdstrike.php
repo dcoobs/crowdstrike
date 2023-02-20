3c64a23da275b038374512305c02482176b79b89
ofirgalcon <ofir@gal.co.uk>
2023-02-20 01:19:01 +0000

    migration fix

--- migrations/2023_02_13_152512_crowdstrike.php
+++ migrations/2023_02_13_152512_crowdstrike.php
@@ -3,14 +3,21 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class CrowdstrikeInit extends Migration
+class Crowdstrike extends Migration
 {
+    private $tableName = 'crowdstrike';
+
+    
     public function up()
     {
         $capsule = new Capsule();
-        $capsule::schema()->create('crowdstrike', function (Blueprint $table) {
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
             $table->integer('sensor_active')->nullable();
 
+        });
+        
+        // Create indexes
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
             $table->index('sensor_active');
         });
     }
