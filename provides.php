1a68cf03bd001e4fc55e6bbe32bc8870d8eb08fb
dcoobs <12276948+dcoobs@users.noreply.github.com>
2023-02-28 16:35:54 -0600

    Rename sensor_active to sensor_operational

--- provides.php
+++ provides.php
@@ -9,7 +9,7 @@ return array(
     ),
     'widgets' => array(
         'crowdstrike_installguard' => array('view' => 'crowdstrike_installguard_widget', 'i18n' => 'crowdstrike.installguard-widget'),
-        'crowdstrike_sensor_active' => array('view' => 'crowdstrike_sensor_active_widget', 'i18n' => 'crowdstrike.sensor-active-widget'),
+        'crowdstrike_sensor_operational' => array('view' => 'crowdstrike_sensor_operational_widget', 'i18n' => 'crowdstrike.sensor-operational-widget'),
         'crowdstrike_sensor_versions' => array('view' => 'crowdstrike_sensor_versions_widget', 'i18n' => 'sensor-versions-widget'),
        'crowdstrike_sensor_versions_graph' => array('view' => 'crowdstrike_sensor_versions_graph_widget', 'i18n' => 'sensor-versions-widget'),
     ),
